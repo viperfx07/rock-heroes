421e929fa3778f8b9c762c73425900498e3fb6ee
Victor Tanudjaja <victor.tanudjaja@readingroom.com.au>
2015-07-29 16:05:05 +1000

    added sort(desc), box shadow to item, overlay info, removed header

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,24 @@ $(document).ready(function() {
     });
 
     $('.sidr-class-navtop').remove();
+
+    $('.episode--title').fitText(1, {
+        minFontSize: '14px',
+        maxFontSize: '21px'
+    });
+    $('.episode--number').fitText(1.1, {
+        minFontSize: '10px',
+        maxFontSize: '14px'
+    });
+
+    var $container = $('.isotope--container'),
+        $items = $('.isotope--item');
+
+    $items.sort(function(a,b){
+        var num1 = parseInt($(a).find('.episode--number span').text());
+        var num2 = parseInt($(b).find('.episode--number span').text());
+
+        return num2-num1;
+    });
+    $items.detach().appendTo($container);
 });
